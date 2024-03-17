a6e7d57cef07592a9bfade5ec8eca291ce3dcd85
SHEIKH MAJID TARIQUE <shaikhmajid321786@gmail.com>
2024-03-18 03:54:52 +0530

    little bit more

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log("Shaikh MAjid")
+
+// data type in javassist
+
+// 1. primitive data type
+// Number
+// Boolean
+// null  ;-- (typeOf - Object)
+// undefined
+// String
+// Symbol
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof "String");
