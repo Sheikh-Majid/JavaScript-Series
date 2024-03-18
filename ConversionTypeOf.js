a7d41be42ace5a60849fb8a7700bcd629863ab41
SHEIKH MAJID TARIQUE <shaikhmajid321786@gmail.com>
2024-03-19 04:28:27 +0530

    add more in this series

---
+++ ConversionTypeOf.js
@@ -0,0 +1,99 @@
+// Number to String
+// let num = 55;
+// console.log(typeof num);
+// let str = String(num);
+// console.log(str);
+// console.log(typeof str);
+
+
+
+// String to Number
+// let str = "shaikh";
+
+// console.log(typeof str);
+// let num = Number(str)
+
+// console.log(num);
+
+// console.log(typeof num);
+
+
+// boolean conversion'
+
+let num = true;
+
+// console.log(typeof num);
+let num2 = Number(num)
+// console.log(typeof(num2))
+// console.log(num2);
+
+let numm = false;
+
+// console.log(typeof num);
+let num3 = Number(numm)
+// console.log(typeof(num3))
+// console.log(num3);
+
+
+// Note
+
+// 1. Number --> String and type0f --> String
+// 1. String --> NaN and typeof --> number
+// 1. true--> 1 and typeOf --> number
+// 1. false--> 0 and typeof --> number
+
+
+// *******************************  OPeration *******************************
+
+// let a = 10;
+// let b = -a;
+// console.log(b);
+
+
+// console.log(2 + 2);
+// console.log(4 - 2);
+// console.log(2 * 3);
+// console.log(2 ** 3);
+// console.log(10 / 2);
+// console.log(19 % 3);
+
+
+//tricky operator
+// if string first it treat as a String
+// if Number first it treat as a Number
+
+
+// console.log("2" + 2);
+// console.log("2" + 2 +2);
+// console.log( 2 + "2");
+// console.log( 2 + 2+ "2" + 2);
+
+
+
+// dataype comorision
+
+// console.log(2 == "2");
+
+// console.log(2 === "2");
+
+// console.log(2 != "2");
+
+
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+
+
+// console.log(undefined == 0);
+
+// console.log(undefined > 0);
+
+// console.log(undefined < 0);
+
+// console.log(undefined >= 0);
+
+// console.log(undefined <= 0);
+
+
